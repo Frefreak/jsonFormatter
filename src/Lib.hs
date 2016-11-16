a1f2de9b0aa8c17c3c805cef6b806152964fefcb
Xiangyu Zhu <frefreak.zxy@gmail.com>
2016-11-16 16:48:29 +0800

    fix unicode issue

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,13 +6,14 @@ module Lib
 
 import Data.Aeson (eitherDecode, Value(..))
 import Data.Aeson.Encode.Pretty (encodePretty)
+import qualified Data.ByteString.Lazy.UTF8 as U
 import qualified Data.ByteString.Lazy.Char8 as LBS
 
 prettify' :: LBS.ByteString -> LBS.ByteString
 prettify' src = let (val :: Either String Value) = eitherDecode src
                 in case val of
-                    Left err -> LBS.concat ["invalid json: ", LBS.pack err]
+                    Left err -> LBS.pack err
                     Right val' -> encodePretty val'
 
 prettify :: String -> String
-prettify = LBS.unpack . prettify' . LBS.pack
+prettify = U.toString . prettify' . U.fromString
--- src/Webview.hs
+++ src/Webview.hs
@@ -6,11 +6,17 @@ module Webview
 import Reflex.Dom
 import Lib (prettify)
 import Control.Lens ((^.), (&), (.~))
+import qualified Data.Map as M
 
 jsFormatterWidget :: MonadWidget t m => m ()
 jsFormatterWidget = do
-    ta1 <- textArea def
-    ta2Value <- holdDyn "" $ ta1 ^. textArea_input
-    let ta1Value = ta1 ^. textArea_input
-    ta2 <- textArea $ def & textAreaConfig_setValue .~ (prettify <$> ta1Value)
+    {-let taAttr = constDyn $ M.singleton "class" "materialize-textarea"-}
+    divClass "container" $
+        divClass "row" $ do
+            ta1Value <- divClass "input-field col s6" $ do
+                ta1 <- textArea $ def -- & textAreaConfig_attributes .~ taAttr
+                return $ ta1 ^. textArea_input
+            divClass "input-field col s6" $ textArea $
+                    def & textAreaConfig_setValue .~ (prettify <$> ta1Value)
+                        {-& textAreaConfig_attributes .~ taAttr-}
     return ()
