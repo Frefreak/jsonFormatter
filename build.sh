17e35e8c0de8681986d5f08294f4a9240c9d5fbc
Xiangyu Zhu <frefreak.zxy@gmail.com>
2016-11-25 14:02:52 +0800

    optimize js file size

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-stack build && ccjs .stack-work/dist/x86_64-linux/Cabal-1.24.0.0_ghcjs/build/jsonFormatter/jsonFormatter.jsexe/all.js --compilation_level=SIMPLE_OPTIMIZATIONS > jsonFormatter.js
+stack build && ccjs .stack-work/dist/x86_64-linux/Cabal-1.24.0.0_ghcjs/build/jsonFormatter/jsonFormatter.jsexe/all.js --compilation_level=ADVANCED_OPTIMIZATIONS  --externs=node --externs=jquery-3.1.1.min.js > jsonFormatter.js
